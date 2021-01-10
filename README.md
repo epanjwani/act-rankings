78382e55aa036af093df169af7c943b9e55b3cc9
Eashan Panjwani <eashan.panjwani@wustl.edu>
2021-01-09 20:01:27 -0500

    push

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # act-rankings
+
+Main page columns: Rankings, 
+Player page
+Act page
+Track page
+League page
+
+Insert Act page
--- act/ranks/models.py
+++ act/ranks/models.py
@@ -1,34 +1,88 @@
 from django.db import models
 
-# Create your models here.
-
 class Player(models.Model):
     PLAYER_CHOICES = (
         ("Eashan Panjwani", "EP"),
         ("Zach Bell", "ZB"),
+        ("Benner Mullin", "BM"),
+        ("Jackson Brodwolf", "JB"),
+        ("Matt Halper", "MH"),
+        ("Alex Hazel", "AH"),
+        ("Harris Klein", "HK"),
+        ("Kohl Terry", "KT"),
+        ("Alex Mellas", "AM"),
         ("Noah Paige", "NP"),
+        ("Julian Ricci", "JR"),
+        ("Robbie Kramer", "RK"),
+        ("Ethan Roberts", "ER"),
+        ("Henry Rogers", "HR"),
+        ("Ryan Simon", "RS"),
+        ("Peter Netch", "PN"),
+        ("Andrew Schanny", "AS"),
+        ("Avery Friedman", "AF"),
+        ("Josh Greenfield", "JG"),
+        ("Will Spartin", "WS"),
+        ("Will Goldberg", "WG"),
+        ("Brady Sheaffer", "BS"),
+        ("Aman Patil", "AP"),
+        ("Zach Stern", "ZS"),
+        ("Josh Josef", "JJ"),
+        ("Nico Simon", "NS"),
+        ("Sam Schoepke", "SS"),
+        ("James Berthoud", "JBe"),
+        ("Jonathan Nurko", "JN"),
+        ("Spencer Gladstone", "SG"),
+        ("Kion Noori", "KN"),
+        ("Max Marchetto", "MM"),
+        ("Chuck Leonetti", "CL"),
+        ("Matt Flower", "MF"),
+        ("Connor Caz", "CC"),
+        ("Caleb Hughes", "CH"),
+        ("Lukas Steinbock", "LS"),
+        ("Brian Volk", "BS")
     )
     name = models.CharField(max_length=50, choices=PLAYER_CHOICES)
     elo_rating = models.IntegerField()
+    races = models.ManyToManyField(Race)
+    points = models.IntegerField()
+
 
 class Team(models.Model):
     player1 = models.ForeignKey(Player, on_delete=models.CASCADE)
     player2 = models.ForeignKey(Player, on_delete=models.CASCADE)
-    num_points = models.IntegerField()
     CHARACTER_CHOICES = (
-        ("Baby Mario", "Baby Mario"),
-        ("Baby Luigi", "Baby Luigi")
+        ("Baby", "Baby"),
         ("Toad", "Toad"),
-        ("Toadette", "Toadette"),
-        ("Baby Koopa", "Baby Koopa"),
+        ("Koopa", "Koopa"),
+        ("Bowser Jr.", "Bowser Jr."),
         ("Diddy Kong", "Diddy Kong"),
-        #add rest of characters
-
+        ("Medium character", "Medium character"),
+        ("Big character", "Big character")
     )
     character_1 = models.CharField(max_length=50, choices=CHARACTER_CHOICES)
     character_2 = models.CharField(max_length=50, choices=CHARACTER_CHOICES)
+    avg_elo = models.IntegerField()
 
-#class Race(models.Model):
+class Race(models.Model):
+    RACE_CHOICES = (
+        ("Luigi Circuit", "Luigi Circuit"),
+        ("Peach Beach", "Peach Beach"),
+        ("Baby Park", "Baby Park"),
+        ("Dry Dry Desert", "Dry Dry Desert"),
+        ("Mushroom Bridge", "Mushroom Bridge"),
+        ("Mario Circuit", "Mario Circuit"),
+        ("Daisy Cruiser", "Daisy Cruiser"),
+        ("Waluigi Stadium", "Waluigi Stadium"),
+        ("Sherbet Land", "Sherbet Land"),
+        ("Mushroom City", "Mushroom City"),
+        ("Yoshi Circuit", "Yoshi Circuit"),
+        ("DK Mountain", "DK Mountain"),
+        ("Wario Colloseum", "Wario Colloseum"),
+        ("Dino Dino Jungle", "Dino Dino Jungle"),
+        ("Bowser Castle", "Bowser Castle"),
+        ("Rainbow Road", "Rainbow Road")
+    )
+    name = models.CharField(max_length=50, choices=RACE_CHOICES)
 
 class ACT(models.Model):
     date = models.DateTimeField(auto_now=True)
@@ -36,6 +90,7 @@ class ACT(models.Model):
     team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
     team3 = models.ForeignKey(Team, on_delete=models.CASCADE)
     team4 = models.ForeignKey(Team, on_delete=models.CASCADE)
+
 
 
 
